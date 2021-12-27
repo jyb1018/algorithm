3e0967bf120627dfa5e6f21a783f65612e1a3b92
YooBin Jeong <jyb1018@gmail.com>
2021-12-27 23:11:49 +0900

    partial sum

---
+++ boj_11660.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+
+int arr[1025][1025];
+int ps[1025][1025];
+
+int partial_sum(int x1, int y1, int x2, int y2)
+{
+    return ps[x2][y2] - ps[x2][y1 - 1] - ps[x1 - 1][y2] + ps[x1 - 1][y1 - 1];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    freopen("input.txt", "r", stdin);
+    
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++)
+        for (int j = 1; j <= N; j++)
+        {
+            cin >> arr[i][j];
+            ps[i][j] = ps[i-1][j] + ps[i][j-1] - ps[i-1][j-1] + arr[i][j];
+        }
+    
+    for (int i = 0; i < M; i++)
+    {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << partial_sum(x1, y1, x2, y2) << "\n";
+    } 
+    
+    
+    
+
+}
